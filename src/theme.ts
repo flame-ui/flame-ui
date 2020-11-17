cd2c9fef98118ffa99dec1fb3bb9586995eaeb00
Nick <nikolaymuller@gmail.com>
2020-11-17 17:42:36 +0300

    Update theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -5,3 +5,5 @@ export type Theme = DefaultTheme
 export type TextLineHeight = keyof DefaultTheme['lineHeights']
 
 export type TextLetterSpacing = keyof DefaultTheme['letterSpacings']
+
+export type Color = keyof DefaultTheme['colors']
