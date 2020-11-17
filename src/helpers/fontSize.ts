08586626969338c22dc2ab4772ad2a0593094ae6
Nick <nikolaymuller@gmail.com>
2020-11-18 02:10:06 +0300

    Update fontSize.ts

--- src/helpers/fontSize.ts
+++ src/helpers/fontSize.ts
@@ -1,5 +1,5 @@
 import { Theme } from '../theme'
 
 export const fontSize = (fontSizeKey: string | number) => ({ theme }: { theme: Theme }): string => {
-  return theme.fontSizes[fontSizeKey as any]
+  return theme.fontSizes[(fontSizeKey as unknown) as any]
 }
