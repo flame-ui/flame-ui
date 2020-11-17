05a27fb938155ebdef0c299e03182fbbe9819039
Nick <nikolaymuller@gmail.com>
2020-11-17 18:29:07 +0300

    Update preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,10 @@
 import React from 'react'
-import { ThemeProvider, createTheme } from '../src'
-
-import { TEXT_LINE_HEIGHTS, TEXT_LETTER_SPACING } from '../src/components/text/example'
+import { ThemeProvider, theme } from '../src'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   layout: 'centered',
 }
-
-const { theme } = createTheme({
-  lineHeights: TEXT_LINE_HEIGHTS,
-  letterSpacings: TEXT_LETTER_SPACING,
-})
 
 const WithTheme = (Story) => (
   <ThemeProvider theme={theme}>
