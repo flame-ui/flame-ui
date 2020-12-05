cf7755300ba1de85cbc4ac0b056578f647312cba
Nick <nikolaymuller@gmail.com>
2020-12-05 07:47:19 +0300

    Update preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,24 +1,16 @@
 import React from 'react'
 import { css } from 'styled-components'
 
-import { ThemeProvider, GlobalStyles, themeGet } from '../src'
+import { ThemeProvider, themeGet } from '../src'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   layout: 'centered',
 }
 
-const styles = css`
-  *::selection {
-    background-color: ${themeGet('colors.primary')};
-    color: white;
-  }
-`
-
 export const decorators = [
   (Story) => (
     <ThemeProvider>
-      <GlobalStyles styles={styles} />
       <Story />
     </ThemeProvider>
   ),
