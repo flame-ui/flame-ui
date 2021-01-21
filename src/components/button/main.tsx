960ae9d6ddf4f24627299121246bf325d74e4bf9
Nick <nikolaymuller@gmail.com>
2021-01-21 07:47:49 +0300

    fix(button): remove default paddings

--- src/components/button/main.tsx
+++ src/components/button/main.tsx
@@ -14,6 +14,7 @@ export const ButtonElement = styled(Box).attrs(() => ({
   as: 'button',
 }))<ButtonProps>`
   margin: 0;
+  padding: 0;
   border: none;
   background-color: transparent;
   user-select: none;
